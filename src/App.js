48092a42f7c8de51b9ccc79a3fe99c2ad4a751ca
Touqeer Ahmed <badsha14072@gmail.com>
2022-10-04 13:15:47 -0700

    first commiy table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,Fragment } from "react";
+import './App.css' ;
+import initialState from "./Data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App=(props)=>
+{
+  const[input,setinput]=useState(initialState);
+  const[items,setitems]=useState([]);
+
+  const itemevent=(e)=>{
+    return(
+        setinput(e.target.value)
+    )
+  }
+  const additems=(st)=>
+  {
+    return(
+      setitems((olditems)=>
+      {
+        return([...olditems, {
+          st,
+          id: st.length + 1,
+        },])
+      })
+    )
+  }
+
+  return(
+  <>
+  <table>  
+    <tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Course</th>
+  </tr>
+</table>
+    {items.map((itemvalue)=>
+    {
+      return(
+        <Fragment key={itemvalue.id}> 
+        <table>
+  <tr>
+    <td>{itemvalue.fname}</td>
+    <td>{itemvalue.lname}</td>
+    <td>{itemvalue.course}</td>
+  </tr>
+</table>
+</Fragment>)
+    })}
+    
+
+<br/>
+    <input type="text" placeholder="Enter Fname" onChange={itemevent} />
+    <input type="text"  placeholder="Enter Lname" onChange={itemevent} />
+    <input type="text"  placeholder="Enter Course" onChange={itemevent} />
+    <input type="button" value="ADD" onClick={additems} />
+  </>
+)
 }
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,17 @@
+
+const initialState = [
+    {
+      fname: 'John',
+      lname: 'John',
+      course: 'John',
+      id: 1,
+    },
+    {
+      fname: 'Jane',
+      lname: 'John',
+      course: 'John',
+      id: 2,
+    },
+  ];
+
+  export default initialState;
